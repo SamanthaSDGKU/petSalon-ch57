9cb710ba9d8ba1508a5d725b03e5adf3bac0e146
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-04-09 19:41:17 -0700

    object constructor

--- scripts/register.js
+++ scripts/register.js
@@ -1,47 +1,41 @@
-console.log("register");
-//create an array
-let pets=[];
+let pets=[]; // the empty array
+//HTML inputs
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
 
+//object constructor
+function Pet(name,age,gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+}
+
+function register(){
+    // create the newPet
+    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value);
+    //push the newPet
+    pets.push(newPet);
+    //display the pets
+    displayPets();
+}
 //display name function
-function displayNames(){
+function displayPets(){
+    document.getElementById("results").innerHTML="";
     for(let i=0;i<pets.length;i++){
-        document.getElementById("results").innerHTML+=`<li>${pets[i].name}</li>`;// arrayName[position]
+        document.getElementById("results").innerHTML+=`<li>${pets[i].name} - ${pets[i].age}</li>`;// arrayName[position]
     }
 }
 
 function init(){
     //create pet objects
-    let pet1 = {
-        name:"Scooby",
-        age:70,
-        gender:"Male",
-        breed:"Dane"
-    }
-
-    let pet2 = {
-        name:"Scrappy",
-        age:60,
-        gender:"Male",
-        breed:"Mixed"
-    }
-
-    let pet3={
-        name:"Nemo",
-        age:10,
-        gender:"Male",
-        breed:"Fish"
-    }
-   
+    let pet1 = new Pet("Scooby",80,"Male");
+    //create another pet Scrappy
+    let pet2 = new Pet("Scrappy",70,"Male");
     // push the pets in the array
-    pets.push(pet1,pet2,pet3);
+    pets.push(pet1,pet2);
     // display names
-    displayNames();
+    displayPets();
 }
 
 window.onload=init;// wait to render the HTML
-
-
-
-
-
-
